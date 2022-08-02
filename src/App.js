8579707c8daf120e2bd06ac460f8dbc9236d9cfa
Karan Chandekar <karanchandekar431@gmail.com>
2022-08-02 16:47:50 +0530

    card component

--- src/App.js
+++ src/App.js
@@ -19,10 +19,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <CardList name="Karan">
-          {this.state.monsters.map((monster) => (
-            <h1 key={monster.id}>{monster.name}</h1>
-          ))}
+        <CardList monsters={this.state.monsters}>
+          
         </CardList>
       </div>
     );
